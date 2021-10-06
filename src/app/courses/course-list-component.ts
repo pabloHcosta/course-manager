fbbe197f9e18aebb149d65ad598025dca2d9ffd8
pabloHaCosta <pablo.costa01@icloud.com>
2021-10-06 10:44:42 -0300

    Adiciona tabela

--- src/app/courses/course-list-component.ts
+++ src/app/courses/course-list-component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Course } from "./course";
 
 @Component({
   selector: 'app-course-list',
@@ -6,5 +7,30 @@ import { Component } from "@angular/core";
 })
 
 export class CourseListComponet {
+  courses: Course[] = [];
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular: Forms',
+        imgUrl: '',
+        price: 99.99,
+        code: 'XPS-8756',
+        duration: 120,
+        raiting: 5.4,
+        releaseDate:'December,2,2021'
+      },
 
+      {
+        id: 2,
+        name: 'Angular: HTTP',
+        imgUrl: '',
+        price: 45.99,
+        code: 'HTP-7685',
+        duration: 80,
+        raiting: 4,
+        releaseDate:'December,01,2021'
+      }
+    ]
+  }
 }
---
+++ src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course {
+  id!: number;
+  name!: string;
+  imgUrl!: string;
+  price!: number;
+  code!: string;
+  duration!: number;
+  raiting!: number;
+  releaseDate!: string;
+}
